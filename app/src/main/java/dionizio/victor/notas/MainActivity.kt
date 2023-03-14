15f2769f1514c44ec574e15eb667c5df0e39ba83
Victor Dionizio <victor.dionizio28@gmail.com>
2023-03-14 20:47:15 -0300

    Adicionando e configurando a Toolbar

--- app/src/main/java/dionizio/victor/notas/MainActivity.kt
+++ app/src/main/java/dionizio/victor/notas/MainActivity.kt
@@ -2,10 +2,31 @@ package dionizio.victor.notas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+    }
+
+    // Setando menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.menu.menu_principal){
+            //TODO: Chamar tela para nova adicionar
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
